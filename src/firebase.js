386418dfd26ddd446ccd23a638fa1c632d553737
SANGEE <murugancharan245@gmail.com>
2021-02-13 15:20:39 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAL2u97WLPAyoSwxkJ9yBq6vn1qwmZwB94",
+  authDomain: "clone-96a3b.firebaseapp.com",
+  projectId: "clone-96a3b",
+  storageBucket: "clone-96a3b.appspot.com",
+  messagingSenderId: "754325117995",
+  appId: "1:754325117995:web:113549e50215d5114b9e6e",
+  measurementId: "G-MRQE7G43VY"
+};
